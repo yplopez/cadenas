dfaae6571cf24b785c1115216bbcae20b861750a
yplopez <yuney.lopez@iesnervion.es>
2024-02-14 23:45:54 +0100

    avances ejercicios cadenas

--- src/cadenas/Ej5.java
+++ src/cadenas/Ej5.java
@@ -7,28 +7,42 @@ public class Ej5 {
 	public static void main(String[] args) {
 		//Creo la variable frase de tipo String para almacenar una frase
 		String frase;
+		
 		//Creo la variable invierte para llamar la funcion y pasarle el parámetro frase
 		String invierte;
+		
 		//Creo el Scanner para ingreso de datos
 		Scanner sc = new Scanner (System.in);
+		
+		//Pido una frase al usuario
 		System.out.println("Ingresa una frase o palabra: ");
+		
+		//Capturo la frase
 		frase = sc.nextLine();
 		
-		invierte = Ej5.invertida(frase);
+		//Asigno a la variable invierte la función invertida y le paso la frase como parámetro
+		invierte = invertida(frase);
 		
-		System.out.println("Tu frase invertida es: " + invierte);
+		//Muestro por consola la frase invertida
+		System.out.println("Tu frase/palabra invertida es: " + invierte);
+		
+		//Cierro el scanner
+		sc.close();
 
 	}
 	
-	
+	//Creo una función de tipo string que recibe como parámetro un string y retorna otro.
 	public static String invertida (String frase) {
 		
+		//Creo la variable de tipo string palabInvertida para almacenar la palabra o frase al contrario.
 		String palabInvertida="";
 		
+		//Creo un bucle for para recorrer la cadena de atrás para adelante.
 		for(int i= frase.length()-1; i>=0; i--) {
+			//asigno y acumulo cada carácter a la variable con la función charAt
 			palabInvertida += frase.charAt(i);
 		}
-		
+		//retorno la palabra/frase invertida al main donde sea llamada
 		return palabInvertida;
 		
 	}
--- src/cadenas/Ej6.java
+++ src/cadenas/Ej6.java
@@ -5,26 +5,37 @@ import java.util.Scanner;
 public class Ej6 {
 
 	public static void main(String[] args) {
+		//Creo la variable palabra de tipo string para almacenar cada palabra ingresada
 		String palabra = "";
-
+		
+		//Creo la variable grupoPalabras para almacenar la frase resultante de la concatenación de palabras
 		String grupoPalabras = "";
 
+		//Creo el Scanner para ingreso de datos
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Escriba una palabra: ");
-		palabra = sc.next();
 
-		while (!palabra.equalsIgnoreCase("fin")) {
-			
-			grupoPalabras +=palabra+" ";
+		//Creo un while para controlar que cuando se ingrese la palabra fin termine el programa
+		//usando la función equalsIgnoreCase
+		while (!palabra.equalsIgnoreCase("fin")) {			
 
+			//Pido las palabras al usuario.
 			System.out.println("Escriba una palabra: ");
-			palabra = sc.next();
-
 			
-
+			//Almaceno las palabras
+			palabra = sc.next();
+			
+			//Evaluo que no aparezca la palabra fin para no concatenar la palabra fin a la cadena
+			if(!palabra.equalsIgnoreCase("fin")) {
+				
+				//Almaceno la acumulacion de palabras en la variable grupoPalabras
+				grupoPalabras +=palabra+" ";
+				}
 		}
+		
+		//Muestro en consola la frase formada por las palabras
 		System.out.println(grupoPalabras);
+		
+		//cierro el scanner
 		sc.close();
 	}
 
