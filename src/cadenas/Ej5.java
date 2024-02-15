1c3e6cf8c1a96310c69a5590354cfee3127b8903
yplopez <yuney.lopez@iesnervion.es>
2024-02-15 12:07:40 +0100

    string

--- src/cadenas/Ej5.java
+++ src/cadenas/Ej5.java
@@ -9,20 +9,22 @@ public class Ej5 {
 		String frase;
 		//Creo la variable invierte para llamar la funcion y pasarle el parámetro frase
 		String invierte;
-		//Creo el Scanner para ingreso de datos
+		//Creo el Scanner para ingreso de datos por teclado
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Ingresa una frase o palabra: ");
+		//Pido al usuario que ingrese una frase
+		System.out.println("Ingresa una frase: ");
+		//Capturo la información ingresada
 		frase = sc.nextLine();
-		
+		//Llamo la función y le paso la variable frase como parámetro
 		invierte = Ej5.invertida(frase);
-		
+		//Muestro en consola la frase invertida.
 		System.out.println("Tu frase invertida es: " + invierte);
 
 	}
 	
-	
+	//Creo una función de tipo String que retorna un String
 	public static String invertida (String frase) {
-		
+		//Creo una variable para almacenar la frase invertida
 		String palabInvertida="";
 		
 		for(int i= frase.length()-1; i>=0; i--) {
